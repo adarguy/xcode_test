3241b51ec13123755e37e7300add33e617bd5910
Adar Guy <adarguy10@gmail.com>
2017-11-01 10:59:02 -0700

    playing around

--- helloWorld/test2.m
+++ helloWorld/test2.m
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
+int test2(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World! My Name is Adar");
