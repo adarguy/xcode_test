453a5a75cfdfc932cd4f8b89485933be4bd7365b
Adar Guy <adarguy10@gmail.com>
2017-11-01 09:16:11 -0700

    testing github commit/push from cmd line

---
+++ helloWorld/main copy.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  helloWorld
+//
+//  Created by GuyAdar on 2017-10-31.
+//  Copyright © 2017 GuyAdar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World! My Name is Adar");
+    }
+    return 0;
+}
